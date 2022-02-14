b3234f5756e28e15ec55c5fb1c5503bc9a153e34
Alessandro Mitelli <alessandro.mitelli@gmail.com>
2022-02-14 17:42:33 +0100

    wip
    Signed-off-by: Alessandro Mitelli <alessandro.mitelli@gmail.com>

--- components/SideNav/SideNav.test.tsx
+++ components/SideNav/SideNav.test.tsx
@@ -1,11 +1,35 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import { SideNav } from './SideNav';
 
 describe('SideNav', () => {
   it('should render', () => {
-    const { asFragment } = render(<SideNav />);
+    const { asFragment } = render(
+      <SideNav
+        items={[
+          {
+            active: true,
+            name: 'First',
+            order: 1,
+            to: '/docs/1',
+          },
+          {
+            active: false,
+            name: 'Second',
+            order: 2,
+            to: '/docs/2',
+          },
+          {
+            active: false,
+            name: 'Third',
+            order: 3,
+            to: '/docs/3',
+          },
+        ]}
+      />,
+    );
 
+    expect(screen.getAllByRole('link')).toHaveLength(3);
     expect(asFragment()).toMatchSnapshot();
   });
 });
--- components/SideNav/SideNav.tsx
+++ components/SideNav/SideNav.tsx
@@ -1,6 +1,5 @@
 import clsx from 'clsx';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import React from 'react';
 
 import { SideNavItem } from '../../lib/nav';
@@ -9,30 +8,24 @@ interface Props {
   items: SideNavItem[];
 }
 
-export const SideNav: React.VFC<Props> = ({ items }) => {
-  const router = useRouter();
-
-  return (
-    <nav className="fixed">
-      <ul>
-        {items.map(({ to, name, active }, index) => (
-          <li key={index} className={clsx('py-2 px-4', { 'bg-secondary-300 rounded': active })}>
-            <div className="flex items-center">
-              <Link href={`${router.basePath}${to}`}>
-                <a
-                  className={clsx(
-                    active
-                      ? 'font-semibold text-primary-700'
-                      : 'hover:text-white text-secondary-300',
-                  )}
-                >
-                  {name}
-                </a>
-              </Link>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </nav>
-  );
-};
+export const SideNav: React.VFC<Props> = ({ items }) => (
+  <nav className="fixed">
+    <ul>
+      {items.map(({ to, name, active }, index) => (
+        <li key={index} className={clsx('py-2 px-4', { 'bg-secondary-300 rounded': active })}>
+          <div className="flex items-center">
+            <Link href={to}>
+              <a
+                className={clsx(
+                  active ? 'font-semibold text-primary-700' : 'hover:text-white text-secondary-300',
+                )}
+              >
+                {name}
+              </a>
+            </Link>
+          </div>
+        </li>
+      ))}
+    </ul>
+  </nav>
+);
