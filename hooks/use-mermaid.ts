3a9da6230a7a5d88f95515b1465e286e5062ee6a
Alessandro Mitelli <alessandro.mitelli@gmail.com>
2022-02-15 16:47:41 +0100

    wip
    Signed-off-by: Alessandro Mitelli <alessandro.mitelli@gmail.com>

--- hooks/use-mermaid.ts
+++ hooks/use-mermaid.ts
@@ -1,17 +1,12 @@
 import mermaid from 'mermaid';
-import { useEffect, useState } from 'react';
+import { useEffect, useLayoutEffect } from 'react';
 
 export const useMermaid = () => {
-  const [initialized, setInitialized] = useState(false);
-
   useEffect(() => {
     mermaid.initialize({ theme: 'dark' });
-    setInitialized(true);
   }, []);
 
-  useEffect(() => {
-    if (initialized) {
-      mermaid.init('.mermaid');
-    }
-  }, [initialized]);
+  useLayoutEffect(() => {
+    mermaid.init('.mermaid');
+  });
 };
