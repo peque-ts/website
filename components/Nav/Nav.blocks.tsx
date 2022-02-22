3ce7407c7b09a90a5dc53dfd82a5769b0551a3a1
Alessandro Mitelli <alessandro.mitelli@gmail.com>
2022-02-22 10:45:39 +0100

    reuse nav items rendering logic

--- components/Nav/Nav.blocks.tsx
+++ components/Nav/Nav.blocks.tsx
@@ -4,11 +4,19 @@ import React from 'react';
 
 import { NAV_ITEMS, useNavMobileButton } from './Nav.helpers';
 
-interface NavMobileProps {
-  bgClassName?: string;
-}
+const NavItems: React.VFC = () => (
+  <>
+    {NAV_ITEMS.map(({ name, to }, index) => (
+      <li key={index}>
+        <Link href={to}>
+          <a className="text-lg">{name}</a>
+        </Link>
+      </li>
+    ))}
+  </>
+);
 
-const NavMobile: React.VFC<NavMobileProps> = ({ bgClassName }) => {
+const NavMobile: React.VFC<{ bgClassName?: string }> = (props) => {
   const { open, renderButton } = useNavMobileButton();
 
   return (
@@ -17,18 +25,12 @@ const NavMobile: React.VFC<NavMobileProps> = ({ bgClassName }) => {
       <div
         className={clsx(
           'absolute inset-x-0 -z-10 shadow-lg transition',
-          bgClassName,
+          props.bgClassName,
           open ? 'translate-y-0' : '-translate-y-full',
         )}
       >
         <ul className={clsx('flex-col space-y-4 items-center pt-4 pb-8', open ? 'flex' : 'hidden')}>
-          {NAV_ITEMS.map(({ name, to }, index) => (
-            <li key={index}>
-              <Link href={to}>
-                <a className="text-lg">{name}</a>
-              </Link>
-            </li>
-          ))}
+          <NavItems />
         </ul>
       </div>
     </div>
@@ -37,13 +39,7 @@ const NavMobile: React.VFC<NavMobileProps> = ({ bgClassName }) => {
 
 const NavDesktop: React.VFC = () => (
   <ul className="hidden tablet:flex space-x-8" data-testid="NavDesktop">
-    {NAV_ITEMS.map(({ name, to }, index) => (
-      <li key={index}>
-        <Link href={to}>
-          <a className="text-lg">{name}</a>
-        </Link>
-      </li>
-    ))}
+    <NavItems />
   </ul>
 );
 
